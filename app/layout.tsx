7e8aed557e314bc39578d7547b1d5f943f262d59
Cuong Tran <cuongtran392003@gmail.com>
2026-04-20 22:46:00 +0700

    update SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,9 @@ export const metadata: Metadata = {
       'max-image-preview': 'large',
       'max-snippet': -1,
     },
+  },
+  verification: {
+    google: 'pMJBPlYTISfenPZgKYa_4fD65vLigk83lQRDixc74yc',
   }
 };
 
